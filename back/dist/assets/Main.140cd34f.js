2f67d974ddce06fe98d8f932fac1312780ee3771
J.sky <bosichong@qq.com>
2022-12-13 11:02:08 +0800

    发布0.1.0 测试版

---
+++ back/dist/assets/Main.140cd34f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,d as n}from"./index.a51140a0.js";const o={};function r(t,a){return c(),n("div",null," \u540E\u53F0\u9996\u9875 ")}const _=e(o,[["render",r]]);export{_ as default};
--- back/main.py
+++ back/main.py
@@ -22,8 +22,11 @@ from database import Base, engine, get_db
 from api_v1 import router
 import crud
 
-__version__ = "0.0.1"
-description = '''Mini Admin,一个简洁轻快的后台管理框架. 🚀'''
+from fastapi.responses import HTMLResponse  # 响应html
+from fastapi.staticfiles import StaticFiles # 设置静态目录
+
+__version__ = "0.1.0"
+description = '''Mini Admin,一个简洁轻快的后台管理框架.支持拥有多用户组的RBAC管理后台 🚀'''
 
 app = FastAPI(
     title="MiniAdmin",
@@ -54,21 +57,29 @@ app.add_middleware(
 
 app.include_router(router)
 
-DATABASE_KEY = "miniadmin"
+# 静态资源
+app.mount("/dist", StaticFiles(directory=os.path.join(BASE_DIR, 'dist')), name="dist")
+app.mount("/assets", StaticFiles(directory=os.path.join(BASE_DIR, 'dist/assets')), name="assets")
+
+
+
 # 删除表，当更新表的结构时可以使用，但是会删除所有数据。慎用！！！！
 # models.Base.metadata.drop_all(bind=engine)
 # 在数据库中生成表结构
 Base.metadata.create_all(bind=engine)
-
+# 生成初始化数据，添加了一个超级管理员并赋予所有管理权限，以及一些虚拟的用户。
 crud.create_data(next(get_db()))
 
 
 @app.get("/")
-def test():
-    return 'Hello MiniAdmin'
+def main():
+    html_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dist', 'index.html')
+    html_content = ''
+    with open(html_path) as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
 
 
 if __name__ == '__main__':
     print('少年，我看你骨骼精奇，是万中无一的编程奇才，有个程序员大佬qq群[217840699]你加下吧!维护世界和平就靠你了！')
     uvicorn.run(app='main:app', host="127.0.0.1", port=8000, reload=True, )
-
